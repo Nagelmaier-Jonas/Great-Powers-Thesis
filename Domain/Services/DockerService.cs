58dd722540790e9de0007b48e0e9bcc19a2137f6
Joni04 <j.nagelmaier@htlkrems.at>
2022-11-26 14:00:28 +0100

    [TASK] added services

---
+++ Domain/Services/DockerService.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Model.Configuration;
+using MySqlConnector;
+
+namespace Domain.Services;
+
+public class DockerService{
+
+    private readonly GreatPowersDbContext _greatPowersDbContext;
+
+    public DockerService(GreatPowersDbContext greatPowersDbContext){
+        _greatPowersDbContext = greatPowersDbContext;
+    }
+
+    public Task StartDockerContainer(string name){
+        var process = new Process();
+        var startInfo = new ProcessStartInfo{
+            WindowStyle = ProcessWindowStyle.Hidden,
+            FileName = "cmd.exe"
+        };
+        process.StartInfo = startInfo;
+        process.StartInfo.Arguments = $"/c cd ..\\Databases\\{name} && docker-compose up -d --build";
+        process.Start();
+        process.WaitForExit();
+        return Task.CompletedTask;
+    }
+    
+    public Task ChangeDbContext(string ipAddress, int port){
+        try{
+            AppSettings.Port = $"{port}";
+            _greatPowersDbContext.Database.CloseConnection();
+            _greatPowersDbContext.Database.SetConnectionString($"server={ipAddress}; port={port}; database=greatpowers; user=greatpowers; password=greatpowers; Persist Security Info=False; Connect Timeout=300");
+            _greatPowersDbContext.Database.OpenConnection();
+            return Task.CompletedTask;
+        }
+        catch (Exception){
+            return Task.CompletedTask;
+        }
+    }
+    
+    public bool CheckConnection(string ipAddress, int port){
+        try{
+            var connection = new MySqlConnection($"server={ipAddress}; port={port}; database=greatpowers; user=greatpowers; password=greatpowers; Persist Security Info=False; Connect Timeout=300");
+            connection.Open();
+            connection.Close();
+            return true;
+        }
+        catch (Exception){
+            return false;
+        }
+    }
+}
---
+++ Model/Configuration/AppSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace Model.Configuration;
+
+public static class AppSettings{
+    public static string Port{ get; set; } = "26280";
+    public static string IpAddress{ get; set; } = "localhost";
+}
