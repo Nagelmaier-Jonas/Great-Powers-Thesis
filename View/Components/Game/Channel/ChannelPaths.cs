6d650332335a11faf43a3622458b5edb516b0b26
Joni04 <j.nagelmaier@htlkrems.at>
2023-01-04 20:56:52 +0100

    [TASK] added last channel

--- View/Components/Game/Channel/ChannelPaths.cs
+++ View/Components/Game/Channel/ChannelPaths.cs
@@ -5,6 +5,6 @@ namespace View.Components.Game.Channel;
 public class ChannelPaths : Dictionary<EChannel, string>{
     public ChannelPaths(){
         this[EChannel.PanamaCanal] = "M108 558 111 560 94 579 91 577 108 558";
-        this[EChannel.SuezCanal] = "";
+        this[EChannel.SuezCanal] = "M742 568 747 568 754 609 749 609 742 568";
     }
 }
